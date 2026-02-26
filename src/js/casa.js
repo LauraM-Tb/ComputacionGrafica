d6e0d4f6cbd7c725415b59147e8cc13b15c6fa84
LauraM-Tb <tabaresviera@gmail.com>
2026-02-26 11:56:06 -0500

    asas

--- src/js/casa.js
+++ src/js/casa.js
@@ -1,3 +1,4 @@
+
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
@@ -22,23 +23,33 @@ camera.position.set(6, 5, 6);
 controls.update();
 
 //////////////////////////////////////////////////////////////
-// CASA
 
-// Base
+// Base 
 const base = new THREE.Mesh(
   new THREE.BoxGeometry(3, 2, 3),
   new THREE.MeshBasicMaterial({ color: 0x00008B }) 
 );
 scene.add(base);
 
-// Techo
+// Techo 
 const roof = new THREE.Mesh(
   new THREE.ConeGeometry(2.5, 1.5, 4),
-  new THREE.MeshBasicMaterial({ color: 0x000000 }) // Negro
+  new THREE.MeshBasicMaterial({ color: 0x000000 })
 );
 roof.position.y = 1.75;
 roof.rotation.y = Math.PI / 4;
 scene.add(roof);
+
+// Puerta 
+const puerta = new THREE.Mesh(
+  new THREE.BoxGeometry(0.8, 1.2, 0.1),
+  new THREE.MeshBasicMaterial({ color: 0x000000 })
+);
+
+// Posicion Puerta
+puerta.position.set(0, -0.4, 1.55);
+
+scene.add(puerta);
 
 //////////////////////////////////////////////////////////////
 
@@ -54,14 +65,4 @@ scene.add(axesHelper);
 function animate() {
   controls.update();
   renderer.render(scene, camera);
-}
-
-//////////////////////////////////////////////////////////////
-
-window.addEventListener('resize', onWindowResize);
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
 }
